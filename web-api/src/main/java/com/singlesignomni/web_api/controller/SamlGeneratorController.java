026f39a166485864ab1ee3f81409a2913463d39c
Robert McLean <robertmcleanh@gmail.com>
2025-03-17 20:58:55 -0400

    allow localhost requests to server

--- web-api/src/main/java/com/singlesignomni/web_api/controller/SamlGeneratorController.java
+++ web-api/src/main/java/com/singlesignomni/web_api/controller/SamlGeneratorController.java
@@ -1,19 +1,13 @@
 package com.singlesignomni.web_api.controller;
 
 import com.singlesignomni.web_api.service.SamlResponseGeneratorV2;
-// import com.singlesignomni.web_api.service.SamlResponseGenerator.Arguments;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SamlGeneratorController {
-    // @Autowired
-    // private SamlResponseGeneratorV2 generator;
-
     @PostMapping("/generate")
     public String generateSamlResponse(
             @RequestBody SamlResponseGeneratorV2.Arguments arguments) {
