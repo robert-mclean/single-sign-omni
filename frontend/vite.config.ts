e019fea2bdabcb861021c26a5135e0c5656c6e60
Robert McLean <robertmcleanh@gmail.com>
2025-03-25 16:10:43 -0400

    update dist path

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.isPages ? "single-sign-omni" : "",
+  base: process.env.isPages ? '/single-sign-omni' : '',
 });
